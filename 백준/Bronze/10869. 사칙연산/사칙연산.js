a600e025b12498993f50d126887f7fca7218214e
SUBIN <134045937+subinsong01@users.noreply.github.com>
2024-01-25 15:15:44 +0900

    [Bronze V] Title: 사칙연산, Time: 136 ms, Memory: 9612 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,16 @@
+//두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오. 
+
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let line = input[0].split(' ');
+
+let a = parseInt(line[0]);
+let b = parseInt(line[1]);
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(parseInt(a / b));
+console.log(a % b);
+
