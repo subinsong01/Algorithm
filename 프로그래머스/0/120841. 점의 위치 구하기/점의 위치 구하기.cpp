fec70374dd5bcbefec027921294008f33fedfa41
SUBIN <134045937+subinsong01@users.noreply.github.com>
2024-03-04 17:19:27 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> dot) {
+    int answer = 0;
+    
+    int x = dot[0];
+    int y = dot[1];
+    
+    if (x > 0 && y > 0) answer = 1;
+    else if (x < 0 && y > 0) answer = 2;
+    else if(x < 0 && y < 0) answer =3;
+    else if (x > 0 && y < 0) answer = 4;
+    
+    return answer;
+}
