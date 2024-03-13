5585d47a085c2563e3c11c5ccf83f0cb1765d55c
SUBIN <134045937+subinsong01@users.noreply.github.com>
2024-03-13 11:22:21 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.01 ms, Memory: 7.72 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -0,0 +1,27 @@
+def solution(s):
+    word_to_num = {
+        "zero": "0",
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9"
+    }
+    
+    result = ""
+    temp = ""
+    
+    for char in s:
+        if char.isdigit():
+            result += char  
+        else:  
+            temp += char  
+            if temp in word_to_num:  
+                result += word_to_num[temp] 
+                temp = "" 
+                
+    return int(result)
