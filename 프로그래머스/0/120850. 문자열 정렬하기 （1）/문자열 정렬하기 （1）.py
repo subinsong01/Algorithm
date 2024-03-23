0e2dc591fde48631c3896403fffec215f5cc7482
SUBIN <134045937+subinsong01@users.noreply.github.com>
2024-03-23 15:01:47 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
@@ -0,0 +1,11 @@
+def solution(my_string):
+    answer = []
+    mylist = list(my_string)
+    
+    for i in mylist:
+        if i.isdigit():
+            answer.append(int(i))
+    
+    answer.sort()
+    
+    return answer
