fd144c8e08cd3ef0659581f6542e12a847f41878
SUBIN <134045937+subinsong01@users.noreply.github.com>
2024-04-03 13:22:01 +0900

    [level 1] Title: 행렬의 덧셈, Time: 27.08 ms, Memory: 20.7 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -2,5 +2,5 @@ def solution(arr1, arr2):
     for i in range(len(arr1)): #배열의 행의 개수를 반환
         for j in range(len(arr1[0])): #배열의 열의 개수를 반환
             arr1[i][j]+=arr2[i][j]
- 
+            
     return arr1
