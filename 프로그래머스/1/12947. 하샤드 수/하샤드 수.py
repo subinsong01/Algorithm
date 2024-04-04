a40710290f8d01a809fcc58f9c85dd7ff87c535f
SUBIN <134045937+subinsong01@users.noreply.github.com>
2024-04-04 21:37:14 +0900

    [level 1] Title: 하샤드 수, Time: 0.00 ms, Memory: 7.75 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.py
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.py
@@ -2,10 +2,10 @@ def solution(x):
     answer = True
     x_all = 0 #각 자릿수를 더한 값 
     x_indivi = str(x) #x를 문자열로 반환 
-    for i in x_indivi:
-        x_all += int(i)
+    for i in x_indivi: 
+        x_all += int(i) #x_indivi 각 문자를 숫자로 변환하고 x_all에 더해준다
         
-    if x % x_all:
+    if x % x_all != 0:
         answer = False
     
     return answer 
