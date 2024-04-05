dfacacdc44afb4c4c4c50b201dd163a765d8290c
SUBIN <134045937+subinsong01@users.noreply.github.com>
2024-04-05 15:25:29 +0900

    [level 2] Title: 행렬의 곱셈, Time: 10.16 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -0,0 +1,11 @@
+def solution(arr1, arr2):
+    m, n, r = len(arr1), len(arr1[0]), len(arr2[0])
+    answer = [[0 for _ in range(r)] for _ in range(m)] # m * r 크기의 행렬
+
+    for i in range(m):
+        for k in range(r):
+            for j in range(n):
+                answer[i][k] += arr1[i][j] * arr2[j][k]
+    
+    return answer
+
