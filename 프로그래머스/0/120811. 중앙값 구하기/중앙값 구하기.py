8b0c4b097e5d8c91a900322a22007d8749003e49
SUBIN <134045937+subinsong01@users.noreply.github.com>
2024-04-08 18:04:33 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.py
@@ -0,0 +1,10 @@
+def solution(array):
+    answer = 0
+    array.sort()
+
+    
+    for i in range(len(array)):
+        num = int(len(array) / 2)
+        return array[num] 
+    
+    return answer
