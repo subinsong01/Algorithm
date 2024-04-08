c3c55bbb3b01d1e812ed4ea4b99806d9d695c384
SUBIN <134045937+subinsong01@users.noreply.github.com>
2024-04-08 18:24:52 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -1,9 +1,12 @@
-def solution(array):
+from collections import Counter
 
-    while len(array) != 0:
-        for i, a in enumerate(set(array)):  
-            #set(집합)은 중복값을 무시하고, 원소의 종류를 오름차순 정렬해준다.
-            array.remove(a)
-        if i == 0:
-            return a
-    return -1
+def solution(array):
+    counts = Counter(array)  # 각 요소의 개수를 세기 위해 Counter를 사용합니다.
+    max_count = max(counts.values())  # 가장 큰 개수를 찾습니다.
+    
+    mode = [num for num, count in counts.items() if count == max_count]  # 최빈값들을 찾습니다.
+    
+    if len(mode) > 1:  # 최빈값이 여러 개인 경우
+        return -1
+    else:
+        return mode[0]  # 최빈값이 하나인 경우 그 값을 반환합니다.
