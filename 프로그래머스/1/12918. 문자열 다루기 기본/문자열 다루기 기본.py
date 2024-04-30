d04bc031e4804ce328dc9fcdf5beca15b3035257
SUBIN <134045937+subinsong01@users.noreply.github.com>
2024-04-30 15:38:37 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.00 ms, Memory: 7.74 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -1,3 +1,5 @@
 def solution(s):
-    return s.isdigit() and len(s) in [4, 6]
- #isdigit: 문자열이 숫자로만 이루어져있는지 확인하는 함수 
+    if (len(s) == 4 or len(s) == 6) and s.isdigit():
+        return True
+    else:
+        return False
