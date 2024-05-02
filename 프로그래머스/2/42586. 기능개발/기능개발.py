e4ce203ddaba6ee7f04269e7130de4d7ce0d3dec
SUBIN <134045937+subinsong01@users.noreply.github.com>
2024-05-02 13:42:34 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -0,0 +1,20 @@
+def solution(progresses, speeds):
+    answer = []
+    #큐를 사용해서 풀이 
+    days = 0
+    cnt = 0
+    
+    while len(progresses) > 0:
+        if (progresses[0] + days * speeds[0]) >= 100: 
+            progresses.pop(0)
+            speeds.pop(0)
+            cnt += 1
+            
+        else:
+            if cnt > 0:
+                answer.append(cnt)
+                cnt = 0
+            days +=1 
+    answer.append(cnt)
+        
+    return answer
