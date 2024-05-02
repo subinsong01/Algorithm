e85b60f42410d588bc044e61407a996ef243a662
SUBIN <134045937+subinsong01@users.noreply.github.com>
2024-05-02 13:43:35 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 120.13 ms, Memory: 27.9 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -2,7 +2,7 @@ def solution(arr):
     answer = []
     
     for i in range(len(arr)):
-        if i == 0:
+        if i == 0: #첫 번째 요소 담기 
             answer.append(arr[i])
         elif arr[i] != arr[i-1]:
             answer.append(arr[i])   
