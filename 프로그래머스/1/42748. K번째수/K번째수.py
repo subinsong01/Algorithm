5eb6aa962923865acfcc8fe449689bfca6b5bc72
SUBIN <134045937+subinsong01@users.noreply.github.com>
2024-05-03 21:00:06 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,7 +1,9 @@
 def solution(array, commands):
     answer = []
     
-    for i, j, k in commands:
-        answer.append(sorted(array[i-1:j])[k-1])
-        
+    for i in range(len(commands)):
+        test = array[commands[i][0] - 1: commands[i][1]]
+        test.sort()
+        answer.append(test[commands[i][2]-1])
+        test.clear()
     return answer
