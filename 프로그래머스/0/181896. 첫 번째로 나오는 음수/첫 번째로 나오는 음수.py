00484333792639381bf6d9e077488c8efe2d05e5
SUBIN <134045937+subinsong01@users.noreply.github.com>
2024-05-06 23:47:24 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.py
@@ -1,8 +1,4 @@
 def solution(num_list):
-    answer = -1  
-    
     for i in range(len(num_list)):
-        if num_list[i] < 0:
-            answer = i  
-            break
-    return answer
+        if num_list[i]<0:return i
+    return -1
