e9bd1826fce5d26d09bda03076306dce5a70f5f0
SUBIN <134045937+subinsong01@users.noreply.github.com>
2024-07-23 18:46:39 +0900

    [Silver V] Title: 돌 게임, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.py
+++ 백준/Silver/9655. 돌 게임/돌 게임.py
@@ -1,6 +1,14 @@
 N = int(input())
+dp = [0] * (1001)
+
+dp[1] = 1
+dp[2] = 2
+dp[3] = 1
  
-if N % 2 == 0 :
-    print("CY")
+for n in range(4, N+1):
+    dp[n] = min(dp[n-1], dp[n-3]) + 1
+ 
+if dp[N] % 2 == 1:
+    print('SK')
 else:
-    print("SK")
+    print('CY')
