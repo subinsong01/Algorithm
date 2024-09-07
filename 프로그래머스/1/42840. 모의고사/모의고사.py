37b7f8a880aa0db0a5ea5b428ca6d33302e80390
SUBIN <134045937+subinsong01@users.noreply.github.com>
2024-09-07 23:35:33 +0900

    [level 1] Title: 모의고사, Time: 3.58 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,22 +1,27 @@
 def solution(answers):
-    answer = [0 for i in range(3)]
-
-    man1 = [1,2,3,4,5]
-    man2 = [2,1,2,3,2,4,2,5]
-    man3 = [3,3,1,1,2,2,4,4,5,5]
+    answer = []
+    A = [1, 2, 3, 4, 5]
+    B = [2, 1, 2, 3, 2, 4, 2, 5]
+    C = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     
+    # 각 학생의 정답 맞춘 개수
+    score = [0, 0, 0]
+    
+    # 답안과 패턴 비교
     for i in range(len(answers)):
-        ans = answers[i]
-        if(man1[i%len(man1)] == ans):
-            answer[0] += 1
-        if(man2[i%len(man2)] == ans):
-            answer[1] += 1
-        if(man3[i%len(man3)] == ans):
-            answer[2] += 1     
+        if answers[i] == A[i % len(A)]:
+            score[0] += 1
+        if answers[i] == B[i % len(B)]:
+            score[1] += 1
+        if answers[i] == C[i % len(C)]:
+            score[2] += 1
     
-    result = []
-    for i in range(len(answer)):
-        if(answer[i] == max(answer)):
-            result.append(i+1)
+    # 최고 점수 찾기
+    max_score = max(score)
     
-    return sorted(result)
+    # 최고 점수를 받은 사람들
+    for i in range(3):
+        if score[i] == max_score:
+            answer.append(i + 1)  # A, B, C는 각각 1, 2, 3번
+    
+    return answer
