da78d813ecb89af3394d64fd0afd6a642381e143
SUBIN <134045937+subinsong01@users.noreply.github.com>
2024-09-08 23:50:38 +0900

    [level 1] Title: 음양 더하기, Time: 0.18 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -1,7 +1,7 @@
 def solution(absolutes, signs):
     answer = 0
-    for i in range(len(absolutes)):
-        if signs[i]:
+    for i in range(len(absolutes)): 
+        if signs[i]: 
             answer += absolutes[i]
         else:
             answer -= absolutes[i]
