6405011cb416c2463c21227d85a97593b7eae48f
SUBIN <134045937+subinsong01@users.noreply.github.com>
2025-01-30 16:45:25 +0900

    [Bronze III] Title: 최소, 최대, Time: 456 ms, Memory: 88604 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.js
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.js
@@ -1,20 +1,10 @@
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-let n = Number(input[0]);
-let arr = input[1].split(' ').map(Number);
+let numbers = input[1].split(' ').map(x=>Number(x));
 
-// 배열이 비어있지 않은 경우에만 첫 번째 요소로 초기화
-let minValue = n > 0 ? arr[0] : undefined;
-let maxValue = n > 0 ? arr[0] : undefined;
 
-for (let i = 0; i < n; i++) {
-    if (minValue === undefined || minValue > arr[i]) {
-        minValue = arr[i];
-    }
-    if (maxValue === undefined || maxValue < arr[i]) {
-        maxValue = arr[i];
-    }
-}
+let minValue = numbers.length > 0 ? numbers.reduce((a, b) => Math.min(a, b)) : undefined; 
+let maxValue = numbers.length > 0 ? numbers.reduce((a, b) => Math.max(a, b)) : undefined;
 
-console.log(minValue, maxValue);
+console.log(minValue + " " + maxValue);
