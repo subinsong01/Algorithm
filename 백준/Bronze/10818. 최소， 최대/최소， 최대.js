ad463cf3276adf4eb843e91f1b52d76825d0fc9c
SUBIN <134045937+subinsong01@users.noreply.github.com>
2025-01-30 16:46:38 +0900

    [Bronze III] Title: 최소, 최대, Time: 396 ms, Memory: 95020 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.js
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.js
@@ -1,10 +1,9 @@
 let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 
-let numbers = input[1].split(' ').map(x=>Number(x));
+let numbers = input[1].split(' ').map(Number);
 
+let minValue = Math.min(...numbers);
+let maxValue = Math.max(...numbers);
 
-let minValue = numbers.length > 0 ? numbers.reduce((a, b) => Math.min(a, b)) : undefined; 
-let maxValue = numbers.length > 0 ? numbers.reduce((a, b) => Math.max(a, b)) : undefined;
-
-console.log(minValue + " " + maxValue);
+console.log(minValue, maxValue);
