5119e610d1dcecac95ce85dc9119bb44cc533d5d
SUBIN <134045937+subinsong01@users.noreply.github.com>
2025-02-01 17:12:18 +0900

    [Silver V] Title: 배열 합치기, Time: 1680 ms, Memory: 430036 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.js
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.js
@@ -4,6 +4,20 @@ let [N, M] = file[0].split(" ").map(Number);
 let num1 = file[1].split(" ").map(Number);
 let num2 = file[2].split(" ").map(Number);
 
-let total = [...num1, ...num2]
-let answer = total.sort((a,b) => a - b);
-console.log(answer.join(" "));
+let result = [];
+let i = 0, j = 0;
+
+while (i < N && j < M) {
+    if (num1[i] < num2[j]) {
+        result.push(num1[i]);
+        i++;
+    } else {
+        result.push(num2[j]);
+        j++;
+    }
+}
+
+while (i < N) result.push(num1[i++]);
+while (j < M) result.push(num2[j++]);
+
+console.log(result.join(' '));
