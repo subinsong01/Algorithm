138e40528dbc9ad17ab49f4b3303daced8abedcb
SUBIN <134045937+subinsong01@users.noreply.github.com>
2025-03-06 13:31:28 +0900

    [Silver I] Title: 숨바꼭질, Time: 2320 ms, Memory: 22332 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
@@ -1,26 +1,28 @@
-let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
-let [N, K] = input[0].split(' ').map(Number);
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim();
 
-function bfs(start, target){
-    const MAX = 100000;
-    let queue = [[start,0]];//현재 위치, 이동 횟수
-    let visited = Array(MAX+1).fill(false);
+let [N, K] = input.split(' ').map(Number);
+
+function bfs(start, target) {
+    let max = 100000;
+    let visited = new Array(max + 1).fill(false); // 방문 체크 배열
+    let deque = []; // 덱(Deque) 사용
     
+    deque.unshift([start, 0]); // [현재 위치, 이동 횟수]
     visited[start] = true;
-    
-    while(queue.length > 0){
-        const[current, count] = queue.shift();
-        
-        if(current === target ){
-            return count;
-        }
-        
+
+    while (deque.length !== 0) {
+        let [current, time] = deque.pop(); // 뒤에서 꺼내기 (O(1))
+
+        if (current === target) return time; // 동생 위치에 도착하면 종료
+
         for (let next of [current - 1, current + 1, current * 2]) {
-            if (next >= 0 && next <= MAX && !visited[next]) {
+            if (next >= 0 && next <= max && !visited[next]) { 
+                deque.unshift([next, time + 1]); // 앞에 추가 (O(1))
                 visited[next] = true;
-                queue.push([next, count + 1]);
             }
         }
     }
 }
-console.log(bfs(N, K));
+
+console.log(bfs(N, K)); // 결과 출력
