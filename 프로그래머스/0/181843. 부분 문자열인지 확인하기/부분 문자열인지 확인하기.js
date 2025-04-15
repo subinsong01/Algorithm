0de8db67b46e095fc4339c26127d0c404dde861a
SUBIN <134045937+subinsong01@users.noreply.github.com>
2025-04-15 13:32:44 +0900

    [level 0] Title: 부분 문자열인지 확인하기, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.js
@@ -0,0 +1,11 @@
+function solution(my_string, target) {
+    var answer = 0;
+    
+    if(my_string.includes(target)){
+        return 1;
+    }else{
+        return 0;
+    }
+    
+    return answer;
+}
