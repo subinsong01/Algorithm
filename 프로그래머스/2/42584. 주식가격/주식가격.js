0afda596bfb7dc6c45a6f670855946ca3aeaf79a
SUBIN <134045937+subinsong01@users.noreply.github.com>
2025-06-25 21:11:44 +0900

    [level 2] Title: 주식가격, Time: 31.28 ms, Memory: 41.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -0,0 +1,19 @@
+function solution(prices) {
+  const answer = Array(prices.length).fill(0);
+  const stack = [];
+
+  for (let i = 0; i < prices.length; i++) {
+    while (stack.length && prices[i] < prices[stack[stack.length - 1]]) {
+      const top = stack.pop();
+      answer[top] = i - top;
+    }
+    stack.push(i);
+  }
+
+  while (stack.length) {
+    const top = stack.pop();
+    answer[top] = prices.length - 1 - top;
+  }
+
+  return answer;
+}
